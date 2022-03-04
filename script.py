10de759df1e56c9ab656d1bbdd0518c6bba0effb
KenGreen618 <kenjaman19@gmail.com>
2022-03-04 09:42:22 +0000

    adding python script

---
+++ script.py
@@ -0,0 +1,6 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+trump_google_trends = pd.read_csv("./resources/csv/trump_google_trends.csv")
+print(trump_google_trends)
